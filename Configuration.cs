2d8871b1baaee43345e7592caa8762d938cfb11d
Полина Малахова <91799147+mamssolivkami@users.noreply.github.com>
2022-12-12 05:20:48 +0700

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityModel;
+using IdentityServer4;
+
+namespace Identity
+{
+    public static class Configuration
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("NotesWebAPI", "Web API")
+            };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+           new List<IdentityResource>
+           {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+           };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource("EmployeesWebAPI", "Web API", new []
+                    { JwtClaimTypes.Name})
+                {
+                    Scopes = {"NotesWebAPI"}
+                }
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "employees-web-api",
+                    ClientName = "Employees Web",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    RequirePkce = true,
+                    RedirectUris =
+                    {
+                        "http://.../signin-oidc"
+                    },
+                    AllowedCorsOrigins =
+                    {
+                        "http://..."
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "http:/.../signout-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "EmployeesWebAPI"
+                    },
+                    AllowAccessTokensViaBrowser = true
+                }
+            };
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Identity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Identity.Models;
+using System.IO;
+
+namespace Identity
+{
+    public class Startup
+    {
+        public IConfiguration AppConfiguration { get; }
+
+        public Startup(IConfiguration configuration) =>
+            AppConfiguration = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = AppConfiguration.GetValue<string>("DbConnection");
+
+            services.AddDbContext<AuthDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>(config =>
+            {
+                config.Password.RequiredLength = 8;
+                config.Password.RequireDigit = true;
+                config.Password.RequireNonAlphanumeric = true;
+                config.Password.RequireUppercase = true;
+            })
+                .AddEntityFrameworkStores<AuthDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddIdentityServer()
+                .AddAspNetIdentity<AppUser>()
+                .AddInMemoryApiResources(new List<ApiResource>())
+                .AddInMemoryIdentityResources(new List<IdentityResource>())
+                .AddInMemoryApiScopes(new List<ApiScope>())
+                .AddInMemoryClients(new List<Client>())
+                .AddDeveloperSigningCredential();
+
+            services.ConfigureApplicationCookie(config =>
+            {
+                config.Cookie.Name = "Notes.Identity.Cookie";
+                config.LoginPath = "/Auth/Login";
+                config.LogoutPath = "/Auth/Logout";
+            });
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(env.ContentRootPath, "Styles")),
+                RequestPath = "/styles"
+            });
+            app.UseRouting();
+            app.UseIdentityServer();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
