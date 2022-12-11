f8f297d76f538de2fe7aa9bc9262cdf56c51f8ac
mamssolivkami <polina_malakhova@inbox.ru>
2022-12-11 16:20:05 +0700

    комментарий2

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -38,6 +38,8 @@ namespace WebApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
+
                 });
     }
 }
